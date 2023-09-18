7466ba0abbdec017fdeb5c8462513fe7edbf8002
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-09-18 12:21:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+// Create a reference to the players array
+ const team = players;
 
+// Create a shallow copy of the players array
+	const team1 = players.slice();
+
+// Create a shallow copy of the person object
+const cap1 = Object.assign({}, person);
+
+ // Test by modifying players and person
+players.push("Sarah"); 
+
+person.name = "jane smith";
+
+// Testing the variables
+console.log("team:", team); 
+console.log("team1:", team1);
+console.log("cap1:", cap1); 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
